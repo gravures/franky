0f6cc720c5a312bd20d5119c02346f739d43ff9f
Gilles Coissac <gilles@ideographe.fr>
2026-03-22 18:23:29 +0100

    style(copyright): add copyright header

---
+++ src/franky/themes/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2025 - Gilles Coissac
+# This file is part of franky library.
+#
+# franky is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# franky is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with franky. If not, see <https://www.gnu.org/licenses/>
